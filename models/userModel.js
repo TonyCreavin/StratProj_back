24f8f4a9203511ce273a9ecfd4a6d4dba2aefc49
Tony Creavin <tonycreavin@gmail.com>
2024-07-31 15:46:39 +0200

    changed phone type from string to number

--- models/userModel.js
+++ models/userModel.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
   city: String,
   postcode: String,
   country: String,
-  phone: String,
+  phone: Number,
 });
 
 userSchema.pre('save', async function (next) {
